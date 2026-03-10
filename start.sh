1b4ee3c423c79fb9c81148fbb431ddedabbf6d32
HassnainAli <ahassnainsaleem964@gmail.com>
2026-03-11 02:24:55 +0500

    update for deployment

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Navigate to backend
+# Install Node (if missing)
+if ! command -v node &> /dev/null; then
+  echo "Node.js not found, installing..."
+  curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+  apt-get install -y nodejs
+fi
+
 cd backend
-
-# Install dependencies
 npm install
-
-# Start the server
 npm run start
